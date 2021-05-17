c07b8cfdbbfacb7e9f01a7d4eefb4146891f6fa7
Shreya Mogulothu <shreya.mogulothu@gmail.com>
2021-05-17 09:12:29 -0400

    add form

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ const itemSchema = new mongoose.Schema({
     file: {
         contentType: String,
         data: Buffer,
+    },
+    claimed: Boolean,
+    claimer: {
+        name: String,
+        email: String
     }
 });
 
@@ -25,9 +30,11 @@ app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"))
 app.set("view engine", "ejs");
 
+// ADMIN-SIDE
+
 app.route("/")
 .get(function(req, res) {
-    Item.find(function(err, items) {
+    Item.find({claimed: true}, function(err, items) {
         if (!err) {
             //console.log(items);
             
@@ -50,7 +57,8 @@ app.route("/")
         file: {
             contentType: req.file.mimetype,
             data: fs.readFileSync(path),
-        }
+        },
+        claimed: false,
     })
 
     anItem.save((err) => {
@@ -75,9 +83,11 @@ app.post("/delete", function({body}, res) {
     });
 });
 
+// STUDENT-SIDE
+
 app.route("/look-up")
 .get((req, res) => {
-    Item.find(function(err, items) {
+    Item.find({claimed: false}, function(err, items) {
         if (!err) {
             //console.log(items);
             
@@ -89,36 +99,45 @@ app.route("/look-up")
 })
 .post(({body}, res) => {
     let {type, startDate, endDate, ID} = body; // 5ef66b2f7fbba908b779dc9a
-    if(ID) {
-        Item.findById(ID, (err, items) => {
-            if (!err) {
-                if(items) {
-                    console.log(items);
-                    res.render("look-up", {items: [items]});
-                } else {
-                    console.log(items);
-                    res.render("look-up", {items: []});
-                }
-            } else {
-                console.log(err);
-                res.render("look-up", {items: []});
-            }
-        })
-    } else {
-        if(!startDate) startDate = new Date(0); else startDate = new Date(startDate);
-        if(!endDate) {const now = new Date(); endDate = new Date(); endDate.setYear(now.getFullYear() + 1)} else endDate = new Date(endDate);
-        console.log(startDate);
-        Item.find({name: { $regex: type, $options: "i" }, date: { $gt: startDate.getTime(), $lt: endDate.getTime() }}, function(err, items) {
-            if (!err) {
-                console.log(items);
-                
-                res.render("look-up", {items: items});
-            } else {
-                console.log(err);
-            }
-        });
-    }
+    // if no startdate, set startdate to 0
+    if(!startDate) startDate = new Date(0); else startDate = new Date(startDate);
+    // if no enddate, set enddate to one year from now
+    if(!endDate) {const now = new Date(); endDate = new Date(); endDate.setYear(now.getFullYear() + 1)} else endDate = new Date(endDate);
+    
+    // search for items
+    Item.find({"_id": ID, claimed: false, name: { $regex: type, $options: "i" }, date: { $gt: startDate.getTime(), $lt: endDate.getTime() }}, function(err, items) {
+        if (!err) {
+            console.log(items);
+            
+            res.render("look-up", {items: items});
+        } else {
+            console.log(err);
+        }
+    });
 });
+
+app.route("/claim")
+.get((req, res) => {
+    res.render("claim", {id: ""})
+})
+.post((req, res) => {
+    const {name, email, id} = req.body;
+    Item.updateOne({"_id": req.body.id, "claimed": false}, {"$set" : {"claimed": true, "claimer": {"name": name, "email": email}}}, (err, result) => {
+        if(err) {
+            console.log("something went wrong");
+        } else if(result.nModified) {
+            console.log("updated!", result)
+        } else {
+            console.log("not updated", result);
+        }
+
+        res.redirect("/claim")
+    })
+})
+
+app.get((req, res) => {
+    res.render("claim", {id: req.params.id})
+})
 
 app.listen(3000, (err) => {
     console.log("connected to port 3000");
